417fe28f3fbcb1cbb8790af438f57c5158b0093f
jashton1998 <89229798+jashton1998@users.noreply.github.com>
2021-08-20 11:38:12 +1000

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -39,7 +39,7 @@ var config = {
   
   "startAudioMuted": 9,
   "channelLastN": -1,
-  "websocket": "wss://rooms.mtsrecruit.online:9090/xmpp-websocket",
+  "websocket": "wss://rooms.mtsrecruit.online:443/colibri-ws",
   "adaptiveLastN": false,
   "hosts": {
     "domain": "rooms.mtsrecruit.online",
